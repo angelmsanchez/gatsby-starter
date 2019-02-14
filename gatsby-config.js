ea8ef3a353945b4e9c76de8c4780e1ce579ce537
Angel Munoz <angelmsanchez85@gmail.com>
2019-02-14 13:35:54 +0100

    -add plugins gatsby and up version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Starter`,
+    title: `Gatsby Starter`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    // {
+    //   resolve: 'gatsby-source-wordpress',
+    //   options: {
+    //     baseUrl: 'http://escuelainfantilsaposapito.es/',
+    //     protocol: 'http',
+    //     hostingWPCOM: true,
+    //     useACF: false,
+    //     auth: {
+    //       wpcom_app_clientSecret: '',
+    //       wpcom_app_clientId: '',
+    //       wpcom_user: '',
+    //       wpcom_pass: '',
+    //     },
+    //     verboseOutput: false,
+    //     searchAndReplaceContentUrls: {
+    //       sourceUrl: 'http://escuelainfantilsaposapito.es/',
+    //       replacementUrl: 'http://escuelainfantilsaposapito.es/',
+    //     },
+    //   },
+    // },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
   ],
 }
